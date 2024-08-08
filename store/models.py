5ec341fcdcbe27976962035deb14d1542b7deab0
amegeddon <ameescook@gmail.com>
2024-08-08 19:24:26 +0100

    Apply initial migrations for built-in and custom apps
    - Applied migrations for `contenttypes`, `auth`, and `admin` apps to set up initial database schema.
    - Ran migrations for the custom `store` app:
      - `0001_initial`: Created initial tables for the `store` app models.
      - `0002_rename_price_product_unit_price`: Renamed the `price` field to `unit_price` in the `Product` model.
      - `0003_product_slug`: Added a `slug` field to the `Product` model.

--- store/models.py
+++ store/models.py
@@ -11,9 +11,10 @@ class Collection(models.Model):
     featured_product = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
+    slug = models.SlugField(default='-')
     title = models.CharField(max_length=255)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
