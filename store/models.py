7103f90ed8e77ffc27b66363a692ce4fe8c8e388
amegeddon <ameescook@gmail.com>
2024-08-08 17:59:27 +0100

    feat: Create 'store' and 'tags' apps, and implement models in 'store' app
    - Added 'store' app with the following models:
      - `Promotion`: Represents promotional offers with a description and discount.
      - `Collection`: Represents a collection of products.
      - `Product`: Represents a product with title, description, price, inventory, and relationships to `Collection` and `Promotion`.
      - `Customer`: Represents customers with first name, last name, email, phone, and optional birth date.
      - `Order`: Represents customer orders with payment status and a relationship to `Customer`.
      - `OrderItem`: Represents items within an order, linked to `Order` and `Product`, with quantity and unit price.
      - `Address`: Represents customer addresses, linked to `Customer`.
      - `Cart`: Represents a shopping cart with a creation timestamp.
      - `CartItem`: Represents items in a shopping cart, linked to `Cart` and `Product`, with quantity.

---
+++ store/models.py
@@ -0,0 +1,60 @@
+from django.db import models
+
+# Create your models here.
+
+class Promotion(models.model):
+    description = models.CharField(max_length=255)
+    discount = models.FloatField()
+
+class Collection(models.model):
+    title = models.CharField(max_length=255)
+
+class Product(models.model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    inventory = models.IntegerField()
+    last_update = models.DateTimeField(auto_now=True)
+    collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
+    promotions = models.ManyToManyField(Promotion)
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255) 
+    last_name = models.CharField(max_length=255)
+    email =  models.EmailField(unique=True)
+    phone = models.CharField(max_length=255) 
+    birth_date = models.DateField(null=True)
+    
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'  
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_FAILED = 'F' 
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'), 
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+        (PAYMENT_STATUS_FAILED, 'Failed')
+    ] 
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.PROTECT)
+    product = models.ForeignKey(Product, on_delete=models.PROTECT) 
+    quantity = models.PositiveSmallIntegerField()  
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2) 
+    
+class address(models.Model):
+    street = models.CharField(max_length=255) 
+    city = models.CharField(max_length=255)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE) # Many to one relationship so customer can have multiple delivery addresses  
+   
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True) 
+    
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+        
--- storefront/settings.py
+++ storefront/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'gallery',
     'debug_toolbar',
+    'store',
+    'tags',
 ]
 
 MIDDLEWARE = [
