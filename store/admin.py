462d93992a7320ab2bed4d459ff5baa960db0c28
amegeddon <ameescook@gmail.com>
2024-08-08 21:18:55 +0100

    Add 'inventory_status' field to ProductAdmin to alert when inventory is low

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 from .models import Collection
 from django.contrib import admin
-from .models import Collection, Product, Promotion
+from .models import Collection, Product, Promotion, Customer
 
 
 @admin.register(Collection)
@@ -12,13 +12,26 @@ class CollectionAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'unit_price', 'collection')
+    list_display = ('title', 'unit_price', 'collection', 'inventory_status')
     list_editable = ['unit_price']
+    list_per_page = 15
     fields = ('slug', 'title', 'description', 'unit_price', 'inventory', 'collection', 'promotions')
     filter_horizontal = ('promotions',)
+    
+    
+    def inventory_status (self, product): 
+        if product.inventory < 2:
+            return 'Low'
+        return 'OK'
+    
 
 @admin.register(Promotion)
 class PromotionAdmin(admin.ModelAdmin):
     list_display = ('description', 'discount')
 
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    list_per_page = 15
+    ordering = ['first_name', 'last_name']
 
