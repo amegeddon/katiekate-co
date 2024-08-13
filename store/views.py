8d95fa3c6558712a803b0750ce01007b5f9357e2
amegeddon <ameescook@gmail.com>
2024-08-13 22:07:26 +0100

    Refactor `Product` and `Collection` views to use `ModelViewSet`
    - Replaced custom view logic with `ModelViewSet` for `Product` and `Collection`.

--- store/views.py
+++ store/views.py
@@ -7,23 +7,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 from .models import Product, Collection
 from .serialisers import ProductSerializer, CollectionSerializer
 
 # Create your views here.
-class ProductList(ListCreateAPIView): 
-    queryset =  Product.objects.select_related('collection').all()
+class ProductViewSet(ModelViewSet):
+    queryset =  Product.objects.all()
     serializer_class =  ProductSerializer  
  
     def get_serializer_context(self):
         return {'request': self.request}
   
-  
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
     def delete(self, request, pk): 
         product = get_object_or_404(Product, pk=pk) 
         if product.orderitem_set.count() > 0:
@@ -31,16 +26,10 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
         
-    
-class CollectionList(ListCreateAPIView): 
-    queryset =  Collection.objects.annotate(products_count=Count('products')).all()
-    serializer_class =  CollectionSerializer
- 
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset =  Collection.objects.annotate(
-            products_count=Count('products'))
-    serializer_class = CollectionSerializer
+class CollectionViewSet(ModelViewSet) :
+    queryset =  queryset =  Collection.objects.annotate(
+            products_count=Count('products')).all()
+    serializer_class = CollectionSerializer  
     
     def delete(self, request, pk ):
         collection = get_object_or_404 (Collection, pk= pk)
