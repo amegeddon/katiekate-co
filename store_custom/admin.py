6fbb2b571441bc3bff0f2a1bb1e977bbfdbc6989
amegeddon <ameescook@gmail.com>
2024-08-15 20:12:46 +0100

    Add CartItemSerializer and CartSerializer with total_price calculations
    - Implemented `CartItemSerializer` with nested `SimpleProductSerializer` for product details.
    - Added `total_price` field to `CartItemSerializer` to calculate the total price of each cart item.
    - Implemented `CartSerializer` with nested `CartItemSerializer` for cart items.
    - Added `total_price` field to `CartSerializer` to calculate the total price of the cart.
    - The `total_price` field in `CartItemSerializer` computes the total price for individual items.
    - The `total_price` field in `CartSerializer` aggregates the total price for all items in the cart.
    
    Optimize CartViewset with prefetch_related for related objects
    
    - Added `prefetch_related('items__product')` to CartViewset to optimize database queries.

--- store_custom/admin.py
+++ store_custom/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin  import ProductAdmin
 from tags.models import TaggedItem
-from store.models import Product
+from store.models import Product, CartItem
 # Register your models here.
 
 class TagInline(GenericTabularInline):
@@ -14,3 +14,9 @@ class  CustomProductAdmin(ProductAdmin):
         
 admin.site.unregister(Product)        
 admin.site.register(Product, CustomProductAdmin)
+
+#This CartItemAdmin class must be deleted before deployment, created solely to add items to cart in order to test api's working as expected 
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ('cart', 'product', 'quantity')
+
+admin.site.register(CartItem, CartItemAdmin)
