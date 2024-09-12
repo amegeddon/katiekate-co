64480bbb844bcabd8a6877834ddf9a76901b12e7
amegeddon <ameescook@gmail.com>
2024-09-12 18:24:25 +0100

    Refactor tests to use Model Bakery for collection and product creation

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.models import User
 from rest_framework import status
 from rest_framework.test import APIClient
+from model_bakery import baker
 import pytest
 from store.models import Collection, Product
 
-# Create your tests here. MUST FINISH REFACTORING - API_CLIENT etc 
+# Create your tests here. MUST FINISH REFACTORING - API_CLIENT etc - use model baker to create collections
 
 @pytest.fixture
 def create_collection(api_client):
@@ -50,11 +51,14 @@ class TestListCollection:
     def setUp(self):
         # Create test collection data
         self.client = APIClient()
+        
         Collection.objects.create(title='Test Collection')
     
     def test_list_collections(self):
         client = APIClient()
+       
         response = client.get('/store/collections/')
+       
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data) > 0    
         
@@ -62,25 +66,32 @@ class TestListCollection:
 class TestDeleteCollection: 
     
      def test_delete_collection_as_admin_returns_204(self):
-        self.collection = Collection.objects.create(title='Test Collection')
+        collection = baker.make('Collection')
         client = APIClient()
         client.force_authenticate(user=User(is_staff =True)) 
-        response = client.delete(f'/store/collections/{self.collection.id}/')
+       
+        response = client.delete(f'/store/collections/{collection.id}/')
+       
         assert response.status_code == status.HTTP_204_NO_CONTENT  
         
      def test_delete_collection_as_anonymous__returns_403(self):
-        self.collection = Collection.objects.create(title='Test Collection')
+        collection = baker.make('Collection')
         client = APIClient()
         client.force_authenticate(user={}) 
-        response = client.delete(f'/store/collections/{self.collection.id}/')
+       
+        response = client.delete(f'/store/collections/{collection.id}/')
+       
         assert response.status_code == status.HTTP_403_FORBIDDEN
         
      def test_delete_collection_with_products_as_admin_returns_405(self):
-        self.collection_with_products = Collection.objects.create(title='Collection with Products')
-        Product.objects.create(title='Test Product', unit_price = 10, inventory=5, collection=self.collection_with_products)
+        
+        collection_with_products = baker.make('Collection', title='Collection with Products')
+        baker.make('Product', title='Test Product', unit_price=10, inventory=5, collection=collection_with_products)
         client = APIClient()
         client.force_authenticate(user=User(is_staff =True)) 
-        response = client.delete(f'/store/collections/{self.collection_with_products.id}/')
+       
+        response = client.delete(f'/store/collections/{collection_with_products.id}/')
+       
         assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
         assert response.data['error'] == 'Collection cannot be deleted at this time because it is associated with one or more products'
         
@@ -88,32 +99,38 @@ class TestDeleteCollection:
 class TestUpdateCollection: 
     
      def test_update_collection_as_admin_returns_200(self):
-        self.collection = Collection.objects.create(title='Test Collection')
+        collection = baker.make('Collection')
         client = APIClient()
         client.force_authenticate(user=User(is_staff =True))  
-        response = client.put(f'/store/collections/{self.collection.id}/', {'title': 'Updated Title'})
+        
+        response = client.put(f'/store/collections/{collection.id}/', {'title': 'Updated Title'})
+        
         assert response.status_code == status.HTTP_200_OK
         assert response.data['title'] == 'Updated Title'
 
+     
      def test_update_collection_as_anonymous_returns_403(self):
-        self.collection = Collection.objects.create(title='Test Collection')
+        collection = baker.make('Collection')
         client = APIClient()
         client.force_authenticate(user={})  
-        response = client.put(f'/store/collections/{self.collection.id}/', {'title': 'Updated Title'})
+        
+        response = client.put(f'/store/collections/{collection.id}/', {'title': 'Updated Title'})
+        
         assert response.status_code == status.HTTP_403_FORBIDDEN   
-        
+             
 @pytest.mark.django_db
-class TestRetrieveCollection:         
-      
-      def test_retrieve_collection(self):
-        self.collection = Collection.objects.create(title='Test Collection')
-        client = APIClient()  
-        response = client.get(f'/store/collections/{self.collection.id}/')
+class TestRetrieveCollection:
+    def test_if_collection_exists_returns_200(self, api_client):
+        collection = baker.make(Collection)
+
+        response = api_client.get(f'/store/collections/{collection.id}/')
+
         assert response.status_code == status.HTTP_200_OK
-        assert response.data['title'] == self.collection.title        
-        
-
-
+        assert response.data == {
+            'id': collection.id,
+            'title': collection.title,
+            'products_count': 0
+        }
 
 
                   
