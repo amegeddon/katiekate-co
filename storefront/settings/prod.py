9fa7fae95ce5297116e8851ccebd0d0ea22789d0
amegeddon <ameescook@gmail.com>
2025-03-04 19:48:56 +0000

    Added cloudinary as an external media repository

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,6 +1,9 @@
 import os
 from .common import *
 import dj_database_url
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 
 DEBUG = False
@@ -22,3 +25,13 @@ EMAIL_HOST = os.environ['TRISTI_SMTP_SERVER']
 EMAIL_HOST_USER = os.environ['TRISTI_SMTP_USER'] 
 EMAIL_HOST_PASSWORD = os.environ['TRISTI_SMTP_PASSWORD']  
 DEFAULT_EMAIL_FROM = 'katiekate2021@outlook.com'
+
+
+# **Cloudinary Storage Configuration**
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.environ.get("dbzincfkg"),
+    "API_KEY": os.environ.get("275537512889256"),
+    "API_SECRET": os.environ.get("CL_UpTSW70cmcFL9O_eiDtQeU9xrs"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
